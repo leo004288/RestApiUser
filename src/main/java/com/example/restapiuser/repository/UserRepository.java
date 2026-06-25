9661db0d42734921d3371f1cefbd90e82aa1e634
leo004288 <leo004288@gmail.com>
2026-06-25 17:19:26 +0900

    index.html script

--- src/main/java/com/example/restapiuser/repository/UserRepository.java
+++ src/main/java/com/example/restapiuser/repository/UserRepository.java
@@ -3,6 +3,12 @@ package com.example.restapiuser.repository;
 import com.example.restapiuser.Entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<UserEntity, String> {
 
+
+    List<UserEntity> findAllByOrderByIndateAsc();
+
+    List<UserEntity> findByUseridContainingIgnoreCaseOrderByIndateAsc(String trim);
 }
